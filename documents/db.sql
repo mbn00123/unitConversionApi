107eac78a8d3830e1ec5c6b17721b448c4b8c77b
Bancha Datrungpithak <bancha@iconx.co.th>
2022-12-19 22:33:40 +0700

    Add documents

---
+++ documents/db.sql
@@ -0,0 +1,27 @@
+create table Unit(
+	unitId int AUTO_INCREMENT PRIMARY key,
+	unitName varchar(50),
+	unitValue decimal(18,2)
+);
+
+create table Metric(
+	metricID int AUTO_INCREMENT PRIMARY key,
+	metricName varchar(50),
+	metricValue decimal(18,2),
+	
+	unitId int,
+	FOREIGN KEY (unitId) REFERENCES Unit(unitId)
+);
+
+create table Context(
+	contextId int AUTO_INCREMENT PRIMARY key,
+	contextName varchar(50),
+	countryID varchar(2),
+	unitId int,
+	metricID int,
+	customMetricName varchar(50),
+	customMetricValue decimal(18,2),
+	
+	FOREIGN KEY (unitId) REFERENCES Unit(unitId),
+	FOREIGN KEY (metricID) REFERENCES Metric(metricID)
+);
